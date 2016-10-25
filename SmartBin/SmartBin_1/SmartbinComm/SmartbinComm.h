d29906f2268c691c72ed65d75dc589fc5c278ae9
Ivan Naumovski <inau@itu.dk>
2016-10-25 15:53:37 +0200

    added structure for report

--- SmartBin/SmartBin_1/SmartbinComm/SmartbinComm.h
+++ SmartBin/SmartBin_1/SmartbinComm/SmartbinComm.h
@@ -2,13 +2,21 @@
 #DEFINE SMARTBINCOMM_H
 #include "arduino.h"
 
+#define PROX 0
+#define GAS_00 1
+#define GAS_02 2
+
+#define ERROR -1
+#define OK 0
+
 class SmartBinComm {
 	private:
 		
 	public:
 		SmartBinComm();
-		void SendValues(String[] labels, float[] values);
-		void SendSensorState(int value);
+		void SendSensorLabels();
+		void SendGasValues(String[] labels, float[] values);
+		void SendSensorState(int sensor, int value);
 }
 
 #endif
